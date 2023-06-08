99481ec327af727558a21e1b8d62d0f240d2e66d
Singh <sabhisheksignh343104@gmail.com>
2023-06-08 16:08:26 +0530

    Changed variables' name

--- tax_deduction.c
+++ tax_deduction.c
@@ -3,17 +3,17 @@
 int main()
 {
     printf("\n\t********* Calculate Net Salary **********\n\n");
-    float bs, tax, ns;
+    float base_salary, tax, net_salary;
     printf("Enter Basic Salary: ");
-    scanf("%f", &bs);
+    scanf("%f", &base_salary);
     getchar();
-    if (bs >= 10000){
-        tax = bs*0.15;
-        ns = bs - tax;
+    if (base_salary >= 10000){
+        tax = base_salary*0.15;
+        net_salary = base_salary - tax;
     }
-    else if (bs < 10000){
-        ns = bs;
+    else if (base_salary < 10000){
+        net_salary = base_salary;
     }
-    printf("\nNet Salary is %0.3f\n", ns);
+    printf("\nNet Salary is %0.3f\n", net_salary);
     return 0;
 }
